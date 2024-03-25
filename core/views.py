42680455b99853d76bfc36ee47fe8be7ce97ee7c
FreZYa <ahmetresul159@gmail.com>
2024-03-25 23:15:02 +0300

    login işlemleri

--- core/views.py
+++ core/views.py
@@ -3,11 +3,17 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
+@login_required(login_url='signin')
+def settings(request):
+    return render(request, "setting.html")
 
 def signup(request):
     if request.method == "POST":
@@ -26,10 +32,13 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('login')
+                return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
@@ -37,4 +46,23 @@ def signup(request):
         return render(request, 'signup.html')
     
 def signin(request):
-    return render(request, "signin.html")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print("username", username)
+        print("password", password)
+        user = auth.authenticate(username=username, password=password)
+        print("user", user)
+        if user:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+    else:
+        return render(request, "signin.html")
+    
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect("signin")
--- social_book/settings.py
+++ social_book/settings.py
@@ -129,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+if DEBUG:
+    INSTALLED_APPS.append('django_extensions')
